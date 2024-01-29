6dfe88aedcc8e821db0206b760ae40c66db5b02b
wgoulet <wgoulet@gmail.com>
2024-01-29 14:18:44 +0000

    Added support for triggering events when new log entries found

--- event-processor.py
+++ event-processor.py
@@ -4,6 +4,25 @@ import csv
 from systemd import journal
 from datetime import datetime, timedelta
 import json
+import pprint
+import os
+from pubsub import pub
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
+
+def listen_kc_event(event):
+    if('operationType' in event.keys()):
+        process_user_event(event)
+
+def process_user_event(event):
+    client_id = os.environ.get('CLIENT_ID')
+    client_secret = os.environ.get('CLIENT_SECRET')
+    token_url = os.environ.get('TOKEN_URL')
+    client = BackendApplicationClient(client_id=client_id)
+    oauth = OAuth2Session(client=client)
+    token = oauth.fetch_token(token_url=token_url,client_id=client_id,client_secret=client_secret)
+    pprint.pprint(token)
+    pprint.pprint(event)
 
 def read_journald_logs(since=None, until=None, unit=None):
     # Read log entries from journald that are created by keycloak. 
@@ -15,13 +34,10 @@ def read_journald_logs(since=None, until=None, unit=None):
     # and convert them into objects that we can feed into a SCIM client.
     poller = select.poll()
     poller.register(reader,reader.get_events())
-    logobjects = []
     while(True):
         poller.poll()
-        #event = reader.get_next()
         for entry in reader:
             if('SYSLOG_IDENTIFIER' in entry.keys()):
-                #print(entry['SYSLOG_IDENTIFIER'])
                 if entry['SYSLOG_IDENTIFIER'] == 'kc.sh':
                     try:
                         obj = json.loads(entry['MESSAGE'])
@@ -38,11 +54,12 @@ def read_journald_logs(since=None, until=None, unit=None):
                                     for entry in row:
                                         (key,value) = entry.split('=')
                                         logobject[key] = value
-                                    logobjects.append(logobject)
+                                    # Publish an event with the created object when we find a keycloak event of interest
+                                    pub.sendMessage('rootTopic',event=logobject)
                     except json.decoder.JSONDecodeError as err:
-                        err
                         print("Not in json format")
                         print(err.doc)
                         continue
-        logobjects
+
+pub.subscribe(listen_kc_event,'rootTopic')
 read_journald_logs()
