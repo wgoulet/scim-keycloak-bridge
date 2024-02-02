0531a36b5b5cd1581b5919d80f8d5d74706345e0
wgoulet <wgoulet@gmail.com>
2024-02-02 22:54:13 +0000

    First version of listener with rabbitmq integration and e2e comms between listener and python working

--- scim-event-listener/src/main/java/com/wgoulet/SCIMEventListenerProvider.java
+++ scim-event-listener/src/main/java/com/wgoulet/SCIMEventListenerProvider.java
@@ -1,11 +1,20 @@
 package com.wgoulet;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 import org.jboss.logging.Logger;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.events.admin.OperationType;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmProvider;
+import org.keycloak.models.UserModel;
+import com.rabbitmq.client.*;
+
+import com.rabbitmq.client.ConnectionFactory;
 
 public class SCIMEventListenerProvider implements EventListenerProvider {
 
@@ -26,9 +35,34 @@ public class SCIMEventListenerProvider implements EventListenerProvider {
 
     @Override
     public void onEvent(AdminEvent adminEvent, boolean b) {
-        System.out.println("About to log event!");
-        System.out.println(adminEvent.getRepresentation());
-        System.out.println("Event logged!!");
+        String rabbitmqpwd = System.getenv("RABBITMQPWD");
+        String rabbitmquname = System.getenv("RABBITMQUNAME");
+        String rabbitmqvhost = System.getenv("RABBITMQVHOST");
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection;
+        try {
+            factory.setUsername(rabbitmquname);
+            factory.setPassword(rabbitmqpwd);
+            factory.setVirtualHost(rabbitmqvhost);
+            connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare("scimbridge", false, false, false, null);
+            if(adminEvent.getOperationType() != OperationType.DELETE) {
+                channel.basicPublish("", "scimbridge", null, adminEvent.getRepresentation().getBytes());
+            }
+            else {
+                channel.basicPublish("", "scimbridge", null, "User is being deleted".getBytes());
+            }
+            System.out.println("About to log event!");
+            System.out.println(adminEvent.getRepresentation());
+            System.out.println("Event logged!!");
+        } catch (IOException | TimeoutException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Fatal error!");
+            e.printStackTrace();
+        }
+       
 
     }
 
--- scim-event-listener/src/main/java/com/wgoulet/SCIMEventListenerProviderFactory.java
+++ scim-event-listener/src/main/java/com/wgoulet/SCIMEventListenerProviderFactory.java
@@ -7,9 +7,18 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.models.UserModel;
 import java.util.Map;
+import java.util.concurrent.TimeoutException;
+import java.io.IOException;
 import java.util.List;
+import com.rabbitmq.client.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wgoulet.DetailDeletedUser;
 
 public class SCIMEventListenerProviderFactory implements EventListenerProviderFactory {
+    private final ConnectionFactory factory = new ConnectionFactory();
+    private Connection connection;
+    private Channel channel;
+
     @Override
     public EventListenerProvider create(KeycloakSession keycloakSession) {
         return new SCIMEventListenerProvider(keycloakSession);
@@ -17,30 +26,53 @@ public class SCIMEventListenerProviderFactory implements EventListenerProviderFa
 
     @Override
     public void init(Config.Scope scope) {
-
+        String rabbitmqpwd = System.getenv("RABBITMQPWD");
+        String rabbitmquname = System.getenv("RABBITMQUNAME");
+        String rabbitmqvhost = System.getenv("RABBITMQVHOST");
+        factory.setHost("localhost");
+        factory.setUsername(rabbitmquname);
+        factory.setPassword(rabbitmqpwd);
+        factory.setVirtualHost(rabbitmqvhost);
+        try {
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+            channel.queueDeclare("scimbridge", false, false, false, null);
+        } catch (IOException | TimeoutException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
-        keycloakSessionFactory.register(
-            (event) -> {
-                if (event instanceof UserModel.UserRemovedEvent) {
-                    UserModel.UserRemovedEvent dEvent = (UserModel.UserRemovedEvent) event;
-                    System.out.println("Got delete event, going to delete this user");
-                    System.out.println(dEvent.getUser().getEmail());
-
-                    Map<String,List<String>> attributes = dEvent.getUser().getAttributes();
-                    for (Map.Entry<String,List<String>> entry : attributes.entrySet())  
-                    {
-                        System.out.println("Key = " + entry.getKey());  
-                        for(String val : entry.getValue()){
-                            System.out.println(val);
-                        }
-                    }
-                    System.out.println("Done logging info about user!");
-                    //TODO YOUR LOGIC WITH `dEvent.getUser()`
-                }
-            });
+            keycloakSessionFactory.register(
+                    (event) -> {
+                            System.out.println("Got an event, going to do something");
+                            if (event instanceof UserModel.UserRemovedEvent) {
+                                UserModel.UserRemovedEvent dEvent = (UserModel.UserRemovedEvent) event;
+                                System.out.println("Got delete event, going to delete this user");
+                                System.out.println(dEvent.getUser().getEmail());
+                                try {
+                                    ObjectMapper mapper = new ObjectMapper();
+                                    DetailDeletedUser duser = new DetailDeletedUser(dEvent,dEvent.getUser());
+                                    byte[] userObj = mapper.writeValueAsBytes(duser);
+                                    //byte[] userObj = mapper.writeValueAsBytes(dEvent.getUser());
+                                    channel.basicPublish("", "scimbridge", null, userObj);
+                                } catch (IOException e) {
+                                    // TODO Auto-generated catch block
+                                    e.printStackTrace();
+                                }
+                                Map<String, List<String>> attributes = dEvent.getUser().getAttributes();
+                                for (Map.Entry<String, List<String>> entry : attributes.entrySet()) {
+                                    System.out.println("Key = " + entry.getKey());
+                                    for (String val : entry.getValue()) {
+                                        System.out.println(val);
+                                    }
+                                }
+                                System.out.println("Done logging info about user!");
+                                // TODO YOUR LOGIC WITH `dEvent.getUser()`
+                            }
+                    });
     }
 
     @Override
